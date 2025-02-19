f03c913427208501b88d4cb8c9a2edeecd5ef5a2
Kavishka Rathnayake <medhangakavishka06@gmail.com>
2025-02-19 16:59:52 +0530

    1.6 - Search for a Non-Existing Item

---
+++ src/test/java/Search/TC_Search_02.java
@@ -0,0 +1,36 @@
+package Search;
+
+import Pages.HomePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TC_Search_02 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void TC_Search_01(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBrowserVersion("121");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+        driver.get("https://www.mageplaza.com/");
+    }
+    @Test
+    public void TC_Search_01Test() throws InterruptedException {
+        HomePage homePage = new HomePage(driver);
+        homePage.ClickSearchBtn();
+        homePage.setSearchBox("InvalidItem123");
+        Thread.sleep(2000);
+    }
+    @AfterMethod
+    public void CloseBrowser(){
+        driver.quit();
+    }
+}
