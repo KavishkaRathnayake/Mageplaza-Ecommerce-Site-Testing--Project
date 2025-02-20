9b3dbbc585535c467af6a301c5d4911516a98515
Kavishka Rathnayake <medhangakavishka06@gmail.com>
2025-02-20 09:21:56 +0530

    1.7 - Add an Item to the Cart without login

---
+++ src/test/java/Cart/TC_Cart_01.java
@@ -0,0 +1,45 @@
+package Cart;
+
+import Pages.HomePage;
+import Pages.ShopNowPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TC_Cart_01 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void TC_Cart_01(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBrowserVersion("121");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+        driver.get("https://www.mageplaza.com/");
+    }
+    @Test
+    public void TC_Cart_01Test() throws InterruptedException {
+        HomePage homePage = new HomePage(driver);
+        homePage.ClickShopNow();
+
+        ShopNowPage shopNowPage = new ShopNowPage(driver);
+        shopNowPage.ViewProuct01();
+        shopNowPage.SelectInstallation();
+        shopNowPage.ClickAddtoCart();
+
+
+        homePage.ClickCart();
+        Thread.sleep(2000);
+
+    }
+    @AfterMethod
+    public void CloseBrowser(){
+        driver.quit();
+    }
+}
